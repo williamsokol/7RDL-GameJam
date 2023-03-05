598a11c19a8d58328d3e01d0c6044e0ca2af6f2f
unknown <williamsokol0@gmail.com>
2023-03-04 22:51:41 -0800

    made grass rounded on edge, and starting on trees

--- Scripts/TileMap.gd
+++ Scripts/TileMap.gd
@@ -1,69 +1,107 @@
-extends TileMap
+extends Node2D
 
 
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var grid:Array
+
 var mapTileSize:Vector2
+#var refTileMap = get_child(0)
+var layers = []
 #var rng = new Random.
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 	randomize()
-	mapTileSize = get_viewport_rect().size/ cell_size
-	#grid = create_2d_array(mapTileSize.y+1,mapTileSize.x+1,1)
+	layers = get_children()
+	mapTileSize = get_viewport_rect().size/ layers[0].cell_size
 	
-	#print(grid[64][37])
+	print(TileMap.INVALID_CELL)
 	
 	
-	clear()
-	grid = ResetMap()
-	#for i in range(2):
-	#	grid = CellularAutomata(grid)
-	LoadGrid(grid)
+	# Create patchy Grass on map
+	layers[0].clear()
+	var grassGrid:Array
+	grassGrid = RandomizedMap([-1,0])
+	for i in range(2):
+		grassGrid = CellularAutomata(grassGrid,-1)
+	LoadGrid(grassGrid,layers[0])
+	layers[0].update_bitmask_region(Vector2.ZERO, mapTileSize)
 	
-func ResetMap():
+	clearSingleTiles(grassGrid,0)
+	
+	# Create Trees on Grass
+	var treeGrid:Array
+	treeGrid = placeTrees(grassGrid,[-1,3])
+	LoadGrid(treeGrid,layers[2])
+	
+func RandomizedMap(tiles:Array):
 	var result = create_2d_array(mapTileSize.y+1,mapTileSize.x+1,1)
 	for x in mapTileSize.x:
 		for y in mapTileSize.y:
-			var cellType = randi()%(1+1)
+			var cellType = tiles[randi()%tiles.size()]
 			result[x][y] = cellType
 	return result
 #func LoadCell(x,y):
 	
-
-func CellularAutomata(tileGrid):
+# this function is the core idea
+func CellularAutomata(tileGrid,targetTile):
 	var result = create_2d_array(mapTileSize.y+1,mapTileSize.x+1,1)
 	var tempGrid = tileGrid.duplicate(true)
+	
 	for x in mapTileSize.x:
 		for y in mapTileSize.y:
-			var neighborCount = 0
-			for i in range(x-1,x+2):
-				for j in range(y-1,y+2):
-					if(!isWithinBounds(i,j)):
-						continue
-					if( x==i and y==j):
-						continue
-					if(tempGrid[i][j] == 1):
-						neighborCount += 1
-						#print(neighborCount)
+			var neighborCount = GetNeigborCount(tempGrid,x,y,targetTile)
 			
 			if(neighborCount > 4):
-				#set_cell(x,y,1)
-				result[x][y] = 1
+				result[x][y] = targetTile
 			else:
 				result[x][y] = 0
-				#set_cell(x,y,0)
 	return result
+	
 func isWithinBounds(x,y):
 	if(x<0 or y<0 or x>= mapTileSize.x or y>=mapTileSize.y):
 		return false
 	return true
-func LoadGrid(tileGrid):
+
+func clearSingleTiles(tileGrid,targetTile):
+	var tempGrid = tileGrid.duplicate(true)
+	
 	for x in mapTileSize.x:
 		for y in mapTileSize.y:
-			set_cell(x,y,tileGrid[x][y])
+			#var neighborCount = GetNeigborCount(tempGrid,x,y,targetTile)
+			if (tempGrid[x][y] == -1):
+				continue
+			if (layers[0].get_cell_autotile_coord(x,y)==Vector2.ZERO):
+				print(tempGrid[x][y])
+				tempGrid[x][y] = -1
+	return tempGrid
+	
+func placeTrees(tileGrid:Array,tiles:Array):
+	var result = create_2d_array(mapTileSize.y+1,mapTileSize.x+1,1)
+	for x in mapTileSize.x:
+		for y in mapTileSize.y:
+			if (tileGrid[x][y] == -1):
+				continue
+			var cellType = tiles[randi()%tiles.size()]
+			result[x][y] = cellType
+	return result
+func GetNeigborCount(tempGrid,x,y,targetTile):
+	var neighborCount = 0
+	for i in range(x-1,x+2):
+		for j in range(y-1,y+2):
+			if(!isWithinBounds(i,j)):
+				continue
+			if( x==i and y==j):
+				continue
+			if(tempGrid[i][j] == targetTile):
+				neighborCount += 1
+	return neighborCount
+
+func LoadGrid(tileGrid,layer):
+	for x in mapTileSize.x:
+		for y in mapTileSize.y:
+			layer.set_cell(x,y,tileGrid[x][y])
 
 func create_2d_array(width, height, value):
 	var a = []
@@ -77,7 +115,7 @@ func create_2d_array(width, height, value):
 
 	return a
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Input.is_action_just_pressed("Fire"):
-		grid = CellularAutomata(grid)
-		LoadGrid(grid)
+#func _process(delta):
+#	if Input.is_action_just_pressed("Fire"):
+#		grid = CellularAutomata(grid)
+#		LoadGrid(grid)
